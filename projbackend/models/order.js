445ecad6c4f9827a170c68c9ded43836c42b8086
Jitesh sindhare <jitesh.sindhare@gmail.com>
2020-06-03 20:28:09 +0530

    adding order schema

---
+++ projbackend/models/order.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const {ObjectId} = mongoose.Schema; // this is used when we want to refer another
+                            // model(table not exactly but kinda ) from our data.
+// ObjectId in line 2 is surrounded by { } coz we are destructuring ObjectId from 
+// mongoose.Schema. This is a javascript destructuring concept.
+
+// not a good idea to state two schema in one file but still.
+
+//.. There are 4 major ways of writing schemas.
+
+const ProductCartSchema = new mongoose.Schema({
+    //this products are the products created in "product.js"
+    product:{
+        type: ObjectId,
+        ref:"Product" // referring from "Product.js" check last line of prodcut.js.
+    },
+    name: String,
+    count: Number, // number of products a user is ordering.
+    price: Number , // total cost of all the product a user is buying.
+});
+
+const  ProductCart = mongoose.model("ProductCart",ProductCartSchema);
+
+const OrderSchema = new mongoose.Schema({
+    // notice in any websites cart, there are list of products which a
+    // a user buys. so it will be an array/list of products.
+    products:[ProductCartSchema],
+    transaction_id:{},
+    amount: {type: Number},
+    address: String,
+    updated: Date,
+    user:{ // to map products to a user.
+        type: ObjectId,
+        ref: "User" // reference of a model we are refering an Object to.
+    }
+},{timestamps: true
+});
+
+const Order = mongoose.model("Order",OrderSchema);
+// to throw this two schemas "OrderSchema and productCartSchema" at the same time from this 
+// file creating this two variables const Order and const ProductSchema.
+
+module.exports = {Order,ProductCart}
