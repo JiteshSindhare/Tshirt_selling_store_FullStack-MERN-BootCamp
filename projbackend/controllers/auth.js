a56f5c93d406f1e261d92789f2ce981c085066d8
Jitesh sindhare <jitesh.sindhare@gmail.com>
2020-06-07 15:12:50 +0530

    added sample authentication routes and token, Created SignIn toute and sending token in cookie.

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -4,6 +4,9 @@
 // below variable should named as User because we exported User schema with same name.
 const User = require("../models/user");
 const { check, validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+
 
 //module.export just exports a single thing if we want to export more then use below
 // kind of methods.
@@ -22,7 +25,7 @@ exports.signup = (req,res) => {
             "msg": "Invalid value",
             "param": "username"
             }]
-            }
+            }   
          */
         return  res.status(422).json({
             // notice in example errors gives an array. so errors.array() convert that
@@ -55,8 +58,63 @@ exports.signup = (req,res) => {
         });
     });
 };
+
+exports.signin = (req,res) =>{
+    //we just need two information now from req.body, we will extract email and pass.
+    const {email,password} = req.body; // destructuring of data.
+    //Validation check.
+    const errors = validationResult(req)// this is from express-validator
+    if(!errors.isEmpty()){
+        return  res.status(422).json({
+            error: errors.array()[0].msg,
+            error_in: errors.array()[0].param
+        })
+    }
+    //findOne method finds exactly one match from the database
+     // we ahve destructured email above thats why able to pass here directly.
+    User.findOne({email},(err,user) =>{
+        // this callback in such cases always sends back err(-ve response) or user(+ve response).
+        if(err || !user){
+           return res.status(400).json({
+                error:"USER email does not exists."
+            })
+        }
+    // if user is beind authenticated, then we need to provide on what basis, we want to check this authentication( see authenticate method in user.js).
+    if(!user.authenticate(password)){
+        return res.status(400).json({
+            error: "Email and password do not match."
+           })
+        }
+    //if above two cases are fine then we can create token, based on any key value pair.
+    const  token = jwt.sign({_id: user._id},process.env.SECRET)
+    // put token in cookie, COOKIE is like a key:value pair.
+    // res.cookie(key,value,expiry time/date)
+    res.cookie("token",token,{expire : new Date()+99999})
+    //send response to frontend to know if this was an error or success. coz above resposnes
+    // were to the browser.
+    // doing destructuring coz we don't want to send the whole thing(user) coz it has user etc.
+    const {_id,name,email,role} = user;
+    // so that we know which user it is , and what authrotis does it have (role will tell that).
+    return res.json({token,user : {_id,name,email,role}})
+    // so that frontend can set this token in local storage.
+    })
+};
+
 exports.signout =  (req,res) => {  
+    // we have this clearCookie method coz we have cookieParser in app.js
+    res.clearCookie("token");
     res.json({
-    message: "user signout..."    
+    message: "user signout successfully"    
     });
 };   
+
+// Protected Routes
+exports.isSignedIn = expressJwt({ 
+
+    secret: process.env.SECRET,
+    //cookie parser allow us to set some properties in user's browser
+    userProperty: "auth"// it adds this new property inside the request which is userProperty: "auth".
+
+    //This method is a custom middleware and we are not writing "next()" here because
+    // we are writing this in expressJwt and that already has a "next()" method.
+});
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var router = express.Router();
 const { check, validationResult } = require('express-validator'); // check expres validator.
-const {signout,signup} = require("../controllers/auth");
+const {signout,signup,signin,isSignedIn} = require("../controllers/auth");
 // remember .. is to go back in directory
 
 
@@ -13,16 +13,30 @@ router.post("/signup",[
     check("password","should be atleast 3 char").isLength({min : 3}),
     // we can define min and max both inside isLength({...}).
 ],signup);
+
+router.post("/signin",[ // keeping router func as post only, sicne in sign in
+    // we will take email and pass from user.
+    check("email","email is required.").isEmail(),
+    check("password","password field is required").isLength({min : 1}),
+
+],signin
+);
+
 //Example
 //In router.get("/signout",(req,res)=>{}); res is response, so we are 
 //sending a response if someone is visiting route /signout
 // sequence of req,res should be same as in this func.
 //res.send("user signout");
 //So when somebody will go to route "/signout" we will send a callback
-// which is 2nd argument(arrow/lambda func).
+// which is 2nd argument(arrow/lambda func or name of a func in below case).
 router.get("/signout",signout); // we can also repalce singout here
 // and put the value os signout here from (req,res) like shown
 //in example.
+
+router.get("/testroute", isSignedIn, (req,res)=>{
+    // this auth has holds an _id, which was given to the user while siginin
+    res.json(req.auth);// this auth is same as we spelled in userProperty of isSigned in auth.js of controller.
+});
 
 module.exports = router; // links or resquest associated with the router
 // of this file are being thrown by the above code. then we need to import
